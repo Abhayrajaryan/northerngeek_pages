7d4116f344d2ca8cf2ce2f725452fb8968eed325
Abhay <rabhay780@gmail.com>
2026-08-11 12:01:23 +0530

    --

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,23 +1,23 @@
 /* FormSubmit shared behaviour */
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   function init() {
     var forms = document.querySelectorAll('form[action*="formsubmit.co"]');
-    forms.forEach(function(form) {
+    forms.forEach(function (form) {
       var next = form.querySelector('input[name="_next"]');
       if (!next) {
-        next = document.createElement('input');
-        next.type = 'hidden';
-        next.name = '_next';
+        next = document.createElement("input");
+        next.type = "hidden";
+        next.name = "_next";
         form.appendChild(next);
       }
-      next.value = window.location.origin + '/thanks/';
+      next.value = new URL("../thanks/", window.location.href).href;
     });
   }
 
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', init);
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
   } else {
     init();
   }
